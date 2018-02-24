59e49654eca5d1aa120a431972b9af347c256fbd
kokosip <witarko.witarko@gmail.com>
2018-02-25 06:49:50 +0700

    clean up unused code

--- app/code/local/Test/Witarko/Block/List.php
+++ app/code/local/Test/Witarko/Block/List.php
@@ -1,12 +1,7 @@
 <?php
 
 class Test_Witarko_Block_List extends Mage_Catalog_Block_Product_Abstract
-{
-    public function getText()
-    {
-        return "I am inside `" . get_class(). "` block and it is my first custom block ever !";
-    }
-		
+{		
 	public function getProductCollection($sortOrder)
     {
         $rootCategoryId = Mage::app()->getStore()->getRootCategoryId();
